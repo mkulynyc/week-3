60c8be06b5bc51a8543083a424f705c982c5871e
Matthew Kulynych <mkulynyc@iu.edu>
2025-09-15 16:26:52 +0000

    Added exercise 2

--- apputil.py
+++ apputil.py
@@ -1,13 +1,10 @@
 import seaborn as sns
 import pandas as pd
 
-
-# update/add code below ...
-
 """
 Exercise 1: Recursive Fibonacci Function
 
-This function recursively calculates the nth Fibonacci number.
+This fib(n) function recursively calculates the nth Fibonacci number.
 
 Arguments:
 - n: An integer representing the position in the Fibonacci sequence.
@@ -15,6 +12,7 @@ Arguments:
 Returns:
 - The nth Fibonacci number.
 """
+
 def fib(n):
     # Return 0 if n is 0
     if n <= 0:
@@ -29,4 +27,28 @@ def fib(n):
         return fib(n - 1) + fib(n - 2)
     
 
+"""
+Exercise 2: Integer to Binary conversion
 
+This to_binary(x) function recursively converts an integer to binary
+
+Arguments: 
+- x: An integer to be converted to binary.
+
+Returns:
+- A string representing the binary equivalent of the integer.
+"""
+
+def to_binary(x):
+    # Base case 1: if x is 0, return 0
+    if x == 0:
+        return "0"
+    
+    # Base case 2: if x is 1, return 1
+    elif x == 1:
+        return "1"
+    
+    # Recursive case: integer divide x by 2 and append the remainder of x divided by 2
+    else:
+        return to_binary(x // 2) + str(x % 2)
+    
